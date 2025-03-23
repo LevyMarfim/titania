1c2cb9ad30f11e9354ccc311672ca219669aece0
Levy Marfim <levymarfim@live.com>
2025-03-23 11:55:52 -0300

    chore: extend base twig template

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class Home
+class Home extends AbstractController
 {
     #[Route('/')]
     public function home(): Response
     {
-        return new Response('<h1>Hello world!</h1>');
+        $gothicNumber = 528;
+
+        return $this->render('main/home.html.twig', [
+            'gothNum' => $gothicNumber,
+        ]);
     }
 }
